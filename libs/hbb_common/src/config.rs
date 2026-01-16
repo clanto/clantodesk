b236e9a202881627b9c55a43a71098c8d9e0cead
AmodioRaffaeleMerone <amodio@clanto.it>
2026-01-16 13:29:15 +0000

    mod

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -109,11 +109,12 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => PUBLIC_RS_PUB_KEY,
+    _ => "",
 };
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
